dc38edca0494c928e9d9d04b25ec91b0e6fd336c
Sorridi <45697827+Sorridi@users.noreply.github.com>
2021-01-10 12:08:29 +0100

    Production ready

--- src/main/java/net/herospvp/combo/Main.java
+++ src/main/java/net/herospvp/combo/Main.java
@@ -1,15 +1,20 @@
 package net.herospvp.combo;
 
+import net.herospvp.base_ffa.Memory;
 import net.herospvp.base_ffa.configuration.CombatTagConfiguration;
 import net.herospvp.base_ffa.configuration.DatabaseConfiguration;
 import net.herospvp.base_ffa.configuration.WorldConfiguration;
 import net.herospvp.base_ffa.configuration.kit.Enchants;
 import net.herospvp.base_ffa.configuration.kit.Kit;
+import net.herospvp.base_ffa.configuration.kit.KitConfiguration;
 import net.herospvp.base_ffa.tasks.ActionBarAnnouncer;
 import net.herospvp.base_ffa.tasks.AlwaysDay;
 import net.herospvp.base_ffa.tasks.SaveData;
+import net.herospvp.combo.events.MoreEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -20,26 +25,25 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
 
+        getServer().getPluginManager().registerEvents(new MoreEvents(), this);
+
         new DatabaseConfiguration(getStringFromConfig("ip"), getStringFromConfig("port"),
-                getStringFromConfig("database"), getStringFromConfig("table"), getStringFromConfig("user"),
+                getStringFromConfig("user"), getStringFromConfig("database"), getStringFromConfig("table"),
                 getStringFromConfig("password"));
 
         new SaveData(18000);
 
-        CombatTagConfiguration.setDuration(10000);
-
-        String[] messages = new String[6];
+        String[] messages = new String[4];
         messages[0] = "&bIP del nostro TeamSpeak: &fts.herospvp.net";
         messages[1] = "&4&lRICORDA! &cIl mouse-abuse non e' consentito!";
         messages[2] = "&bLink del nostro Discord: &fdiscord.gg/6kNTCUjKBg";
         messages[3] = "&6Gestisci le tue notifiche con /notifiche";
-        messages[4] = "qualcosa";
-        messages[5] = "qualcosa";
 
         new ActionBarAnnouncer(messages, 200, 1200);
         new AlwaysDay("combo_ffa", 9000, 1200);
 
         new WorldConfiguration("combo_ffa", null);
+        WorldConfiguration.setPvpDisabledOver(94);
 
         Enchants protection = new Enchants(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
 
@@ -47,10 +51,10 @@ public class Main extends JavaPlugin {
         Enchants durability = new Enchants(Enchantment.DURABILITY, 3);
 
         ItemStack[] armor = new ItemStack[4];
-        armor[0] = new ItemStack(Material.DIAMOND_HELMET);
-        armor[1] = new ItemStack(Material.DIAMOND_CHESTPLATE);
-        armor[2] = new ItemStack(Material.DIAMOND_LEGGINGS);
-        armor[3] = new ItemStack(Material.DIAMOND_BOOTS);
+        armor[3] = new ItemStack(Material.DIAMOND_HELMET);
+        armor[2] = new ItemStack(Material.DIAMOND_CHESTPLATE);
+        armor[1] = new ItemStack(Material.DIAMOND_LEGGINGS);
+        armor[0] = new ItemStack(Material.DIAMOND_BOOTS);
 
         ItemStack[] hotBar = new ItemStack[3];
         hotBar[0] = new ItemStack(Material.DIAMOND_SWORD);
@@ -62,13 +66,33 @@ public class Main extends JavaPlugin {
 
         PotionEffect[] potionEffects = new PotionEffect[2];
         potionEffects[0] = new PotionEffect(PotionEffectType.NIGHT_VISION, 10000000, 0);
-        potionEffects[0] = new PotionEffect(PotionEffectType.SPEED, 10000000, 1);
+        potionEffects[1] = new PotionEffect(PotionEffectType.SPEED, 10000000, 1);
 
         Kit kit = new Kit(armor, hotBar, killReward, potionEffects);
 
         kit.enchantAllArmor(protection);
         kit.enchantHotBar(0, sharpness);
         kit.enchantHotBar(0, durability);
+
+        Memory.setKit(kit);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                short j = 0;
+                for (ItemStack itemStack : player.getInventory()) {
+                    if (itemStack == null) {
+                        j++;
+                        continue;
+                    }
+                    if (hotBar[1].getType() == itemStack.getType() && itemStack.getAmount() != 64 &&
+                            player.getInventory().getItemInHand().getType() != hotBar[1].getType()) {
+                        itemStack.setAmount(64);
+                        player.getInventory().setItem(j, itemStack);
+                    }
+                    j++;
+                }
+            }
+        }, 1200L, 1200L);
     }
 
     @Override
--- src/main/java/net/herospvp/combo/events/MoreEvents.java
+++ src/main/java/net/herospvp/combo/events/MoreEvents.java
@@ -1,38 +1,115 @@
 package net.herospvp.combo.events;
 
 import net.herospvp.base_ffa.Main;
+import net.herospvp.base_ffa.Memory;
 import net.herospvp.base_ffa.configuration.CombatTagConfiguration;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.ItemStack;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoreEvents implements Listener {
 
     @EventHandler
+    public void on(PlayerJoinEvent event) {
+        ePearlCoolDowns.put(event.getPlayer().getName(), 0L);
+    }
+
+    @EventHandler
+    public void on(PlayerQuitEvent event) {
+        ePearlCoolDowns.remove(event.getPlayer().getName());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
     public void on(PlayerDeathEvent event) {
         Player player = event.getEntity();
 
-        if (CombatTagConfiguration.getLastHitter() == null
-                || CombatTagConfiguration.isOutOfCombat(player)) {
+        if (CombatTagConfiguration.isOutOfCombat(player)) {
             return;
         }
         Player killer = CombatTagConfiguration.getLastHitter().get(player);
 
-        killer.getInventory().addItem(Main.getKit().getKillRewards());
+        if (killer == null) return;
+
+        Memory.getKit().setPlayerArmor(player);
 
         if (killer.getItemInHand() == null) return;
 
-        if (killer.getItemInHand().getDurability() != 0) {
+        if (killer.getItemInHand().getDurability() != 0 &&
+                !killer.getItemInHand().equals(Memory.getKit().getHotBar()[1]) &&
+                !killer.getItemInHand().equals(Memory.getKit().getHotBar()[2])) {
             killer.getItemInHand().setDurability((short) 0);
         }
 
     }
 
+    private static final Map<String, Long> ePearlCoolDowns = new HashMap<>();
+
     @EventHandler
+    public void on(PlayerInteractEvent event) {
+        if (event.getItem() == null) return;
+        Event.Result useHandItem = event.useItemInHand();
+
+        if (useHandItem.equals(Event.Result.DENY) || !event.getItem().getType().equals(Material.ENDER_PEARL)) return;
+        if (!(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR))) return;
+
+        Player player = event.getPlayer();
+        String playerName = player.getName();
+
+        long mapGet = ePearlCoolDowns.get(playerName);
+
+        if (mapGet <= System.currentTimeMillis()) {
+            ePearlCoolDowns.replace(playerName, System.currentTimeMillis() + 16000);
+        } else {
+            event.setCancelled(true);
+            DecimalFormat decimalFormat = new DecimalFormat("0");
+
+            player.sendMessage(ChatColor.RED + "Puoi utilizzare la prossima perla tra " +
+                    decimalFormat.format((mapGet - System.currentTimeMillis()) / 1000) + "s");
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
     public void on(PlayerItemConsumeEvent event) {
-        event.getPlayer().getInventory().addItem(event.getItem());
+        Player player = event.getPlayer();
+        ItemStack itemStack = event.getItem();
+        itemStack.setAmount(64);
+
+        int slot = player.getInventory().getHeldItemSlot();
+
+        Bukkit.getScheduler().runTaskLater(Main.getMain(), () -> {
+            if (player.getInventory().getHeldItemSlot() == slot) {
+                return;
+            }
+            player.getInventory().setItem(slot, itemStack);
+        }, 100L);
+    }
+
+    @EventHandler
+    public void on(PlayerTeleportEvent event) {
+        if (event.getCause() == PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
+            Location to = event.getTo();
+            Player player = event.getPlayer();
+
+            if (to.getY() >= 92 || player.getLocation().getY() >= 92) {
+                player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 1));
+                player.sendMessage(ChatColor.RED + "Non puoi teletrasportarti qui!");
+                event.setCancelled(true);
+            }
+        }
     }
 
 }
