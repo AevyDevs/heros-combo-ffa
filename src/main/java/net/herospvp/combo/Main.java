2764b0f02904e78751cf977e10f8776221f3df0f
Sorridi <45697827+Sorridi@users.noreply.github.com>
2021-02-10 20:56:37 +0100

    2.2.0-SNAPSHOT

--- src/main/java/net/herospvp/combo/Main.java
+++ src/main/java/net/herospvp/combo/Main.java
@@ -1,44 +1,45 @@
 package net.herospvp.combo;
 
+import lombok.Getter;
 import net.herospvp.base.Base;
 import net.herospvp.base.commands.Spawn;
 import net.herospvp.base.events.CombatEvents;
 import net.herospvp.base.events.PlayerEvents;
-import net.herospvp.base.utils.lambdas.SpawnLambda;
 import net.herospvp.combo.events.MoreEvents;
+import net.herospvp.heroscore.HerosCore;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+@Getter
 @SuppressWarnings("FieldCanBeLocal")
 public class Main extends JavaPlugin {
 
     private Base base;
-    private Spawn spawn;
-    private PlayerEvents playerEvents;
-    private CombatEvents combatEvents;
+    private HerosCore herosCore;
+    private ItemStack[] armor, hotBar;
 
     @Override
     public void onEnable() {
 
-        base = Base.getInstance();
+        base = getPlugin(Base.class);
+        herosCore = getPlugin(HerosCore.class);
 
         new MoreEvents(this);
 
-        ItemStack[] armor = new ItemStack[4];
+        armor = new ItemStack[4];
         armor[3] = new ItemStack(Material.DIAMOND_HELMET);
         armor[2] = new ItemStack(Material.DIAMOND_CHESTPLATE);
         armor[1] = new ItemStack(Material.DIAMOND_LEGGINGS);
         armor[0] = new ItemStack(Material.DIAMOND_BOOTS);
 
-        ItemStack[] hotBar = new ItemStack[3];
+        hotBar = new ItemStack[3];
         hotBar[0] = new ItemStack(Material.DIAMOND_SWORD);
         hotBar[1] = new ItemStack(Material.ENDER_PEARL, 4);
         hotBar[2] = new ItemStack(Material.GOLDEN_APPLE, 64, (short) 1);
@@ -52,58 +53,7 @@ public class Main extends JavaPlugin {
 
         base.getWorldConfiguration().setPvpDisabledOver(94);
 
-        spawn = base.getSpawn();
-        playerEvents = base.getPlayerEvents();
-        combatEvents = base.getCombatEvents();
-
-        SpawnLambda spawnLambda = (player) -> {
-
-            PlayerInventory playerInventory = player.getInventory();
-            playerInventory.clear();
-            playerInventory.setArmorContents(armor);
-
-            for (int i = 0; i < playerInventory.getContents().length; i++) {
-
-                if (playerInventory.getContents()[i] == null) {
-                    continue;
-                }
-
-                for (ItemStack itemStack : armor) {
-                    if (itemStack.getType().equals(playerInventory.getContents()[i].getType())) {
-                        playerInventory.setItem(i, null);
-                    }
-                }
-            }
-
-            for (int i = 0; i < 3; i++) {
-                player.getInventory().setItem(i, hotBar[i]);
-            }
-
-            Bukkit.getScheduler().runTaskLater(this, () -> {
-                if (player.getActivePotionEffects().size() == 0) {
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 172800000, 0));
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 172800000, 1));
-                } else {
-                    for (PotionEffect effect : player.getActivePotionEffects()) {
-                        if (effect.getType().equals(PotionEffectType.NIGHT_VISION) ||
-                                effect.getType().equals(PotionEffectType.SPEED)) {
-                            continue;
-                        }
-                        player.removePotionEffect(effect.getType());
-                    }
-                }
-            }, 5L);
-        };
-
-        spawn.setSpawnLambda(spawnLambda);
-        playerEvents.setSpawnLambda(spawnLambda);
-        combatEvents.setCombatEventsLambda((player, killer) -> {
-            killer.getInventory().setArmorContents(armor);
-            killer.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 4));
-        });
-
-
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
 
             for (Player player : Bukkit.getOnlinePlayers()) {
 
@@ -136,6 +86,7 @@ public class Main extends JavaPlugin {
                 }
             }
         }, 600L, 600L);
+
     }
 
     @Override
--- src/main/java/net/herospvp/combo/events/MoreEvents.java
+++ src/main/java/net/herospvp/combo/events/MoreEvents.java
@@ -1,6 +1,10 @@
 package net.herospvp.combo.events;
 
+import net.herospvp.base.events.custom.CombatKillEvent;
 import net.herospvp.combo.Main;
+import net.herospvp.heroscore.HerosCore;
+import net.herospvp.heroscore.objects.HPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -11,20 +15,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
+import net.herospvp.base.events.custom.SpawnEvent;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings("FieldCanBeLocal")
 public class MoreEvents implements Listener {
 
     private final Main instance;
+    private final HerosCore herosCore;
     private final Map<String, Long> ePearlCoolDowns;
 
     public MoreEvents(Main instance) {
-        this.instance = instance;
         ePearlCoolDowns = new HashMap<>();
+        this.instance = instance;
+        this.herosCore = instance.getHerosCore();
         instance.getServer().getPluginManager().registerEvents(this, instance);
     }
 
@@ -75,6 +84,69 @@ public class MoreEvents implements Listener {
                 event.setCancelled(true);
             }
         }
+    }
+
+    @EventHandler
+    public void on(SpawnEvent event) {
+        Player player = event.getPlayer();
+
+        PlayerInventory playerInventory = player.getInventory();
+        playerInventory.clear();
+        playerInventory.setArmorContents(instance.getArmor());
+
+        for (int i = 0; i < playerInventory.getContents().length; i++) {
+
+            if (playerInventory.getContents()[i] == null) {
+                continue;
+            }
+
+            for (ItemStack itemStack : instance.getArmor()) {
+                if (itemStack.getType().equals(playerInventory.getContents()[i].getType())) {
+                    playerInventory.setItem(i, null);
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            player.getInventory().setItem(i, instance.getHotBar()[i]);
+        }
+
+        Bukkit.getScheduler().runTaskLater(instance, () -> {
+            if (player.getActivePotionEffects().size() == 0) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 172800000, 0));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 172800000, 1));
+            } else {
+                for (PotionEffect effect : player.getActivePotionEffects()) {
+                    if (effect.getType().equals(PotionEffectType.NIGHT_VISION) ||
+                            effect.getType().equals(PotionEffectType.SPEED)) {
+                        continue;
+                    }
+                    player.removePotionEffect(effect.getType());
+                }
+            }
+        }, 7L);
+
+    }
+
+    @EventHandler
+    public void on(CombatKillEvent event) {
+        Player killer = event.getKiller();
+
+        for (ItemStack itemStack : killer.getInventory()) {
+            if (itemStack == null) continue;
+
+            for (ItemStack stack : instance.getArmor()) {
+                if (stack.getType().equals(itemStack.getType())) {
+                    killer.getInventory().remove(itemStack);
+                }
+            }
+        }
+
+        killer.getInventory().setArmorContents(instance.getArmor());
+        killer.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 4));
+
+        HPlayer hKiller = herosCore.getPlayersHandler().getPlayer(killer.getUniqueId());
+        hKiller.setCoins(hKiller.getCoins() + 5);
     }
 
 }
